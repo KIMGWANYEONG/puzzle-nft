93f1cf1f542d460a63c258043c8d51d08d2487dc
KIMGWANYEONG <pipe0youngk@gmail.com>
2024-06-11 16:38:55 +0900

    font

--- vite/src/main.tsx
+++ vite/src/main.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+import "./index.css";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Yeongdeok Snow Crab",
+    body: "Yeongdeok Snow Crab",
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
